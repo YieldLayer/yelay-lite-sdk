a07867ccac65dfe10aac0e9245d57085c9795235
weeheng123 <weeheng.goh99@gmail.com>
2025-02-27 23:21:34 +0800

    add projectsTVL prop to getProjectsTVL

--- tests/services/projects.test.ts
+++ tests/services/projects.test.ts
@@ -1,5 +1,8 @@
 import { ethers } from 'ethers';
 import { sdkConfig, YelayLiteSdk } from '../../src';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 describe('Projects', () => {
 	let sdk: YelayLiteSdk;
@@ -14,9 +17,10 @@ describe('Projects', () => {
 		sdk = new YelayLiteSdk(signer, sdkConfig[8453]);
 	});
 
-	it('get allowance', async () => {
+	it('get projectsTVL', async () => {
 		const projectTvl = await sdk.projects.getProjectsTvl('0x16db68c86edfdb60ba733563326ed392b319eb2b', [1, 2]);
 
-		console.log('projectTvl', projectTvl.projectsSupply[0].supply.toString());
+		console.log('projectTvl1', projectTvl.projectsTVL[0].tvl.toString());
+		console.log('projectTvl2', projectTvl.projectsTVL[1].tvl.toString());
 	});
 });
