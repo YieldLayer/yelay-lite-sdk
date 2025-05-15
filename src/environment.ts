cbb6d9efb7f75d9245cc928b49b481cbf1815a4f
Igor Trofimov <igortr333@gmail.com>
2025-05-15 15:04:38 +1000

    populate YE addresses

--- src/environment.ts
+++ src/environment.ts
@@ -11,8 +11,7 @@ export const getEnvironment = (chainId: ChainId, testing: boolean): SDKConfig =>
 				contracts: {
 					VaultWrapper: '0xE252b5c05a18140F15E1941dD2Df8a95bDa8A20b',
 					Swapper: '0x769F5bc628CF538E1D64F2e8ED235A8831312249',
-					// TODO:
-					YieldExtractor: '',
+					YieldExtractor: '0xf3b5e160898cfd8b89476e77887050abb377c277',
 				},
 				backendUrl: 'https://lite.dev.yelay.io/v2',
 			};
@@ -21,8 +20,7 @@ export const getEnvironment = (chainId: ChainId, testing: boolean): SDKConfig =>
 				contracts: {
 					VaultWrapper: '0xdccf337ea77b687a4daca5586351b08f8927c825',
 					Swapper: '0xbbc6e62f23f714405d7e0b4d3dde079e22748a58',
-					// TODO:
-					YieldExtractor: '',
+					YieldExtractor: '0x4d6a89dc55d8bacc0cbc3824bd7e44fa051c3958',
 				},
 				backendUrl,
 			};
@@ -33,8 +31,7 @@ export const getEnvironment = (chainId: ChainId, testing: boolean): SDKConfig =>
 			contracts: {
 				VaultWrapper: '0xf65d02700915259602D9105b66401513D1CB61ff',
 				Swapper: '0xD49Dc240CE448BE0513803AB82B85F8484748871',
-				// TODO:
-				YieldExtractor: '',
+				YieldExtractor: '0x226239384EB7d78Cdf279BA6Fb458E2A4945E275',
 			},
 			backendUrl,
 		};
@@ -44,8 +41,7 @@ export const getEnvironment = (chainId: ChainId, testing: boolean): SDKConfig =>
 			contracts: {
 				VaultWrapper: '0x0872e8391662D4e53D6649c8dE5d4bF581Bd778C',
 				Swapper: '0x98732e2FEb854bAd400D4b5336f4439E7E53fe88',
-				// TODO:
-				YieldExtractor: '',
+				YieldExtractor: '0xB84B621D3da3E5e47A1927883C685455Ad731D7C',
 			},
 			backendUrl,
 		};
